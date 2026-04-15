fb3d38e62263c6c7a581c66814f506aa4a801901
pylypchukroman <romanpylypchuk@gmail.com>
2026-04-15 20:28:03 +1000

    еуіе commit

--- admin.php
+++ admin.php
@@ -39,7 +39,8 @@
         <p>A: 111 Business Avenue, TULITZA NSW 9460<br>
         P: 02 0000 0000<br>
         E: contactus@gelosmail.com.au</p>
-    
+
+
         <p>Gelos Enterprises would like to pay our respect and acknowledge Aboriginal and Torres Strait Islander Peoples as the Traditional Custodians of the land, rivers and sea. We acknowledge and pay our respect to the Elders, both past and present of all Nations.</p>				
         <p>This site has been developed by TAFE NSW &copy TAFE NSW <?php echo date("Y") ?></p>
         <p >Please note this is a fictitious organisation and has been created purely for educational purposes only.</p>
